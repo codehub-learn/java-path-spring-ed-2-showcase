fd204dbcbe759dd637c5b2b9d4421c1346681e6f
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-14 09:02:18 +0200

    Renamed package to match the artifact id.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
@@ -1,4 +1,4 @@
-package gr.codelearn.spring.showcase.eshop;
+package gr.codelearn.spring.showcase.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
