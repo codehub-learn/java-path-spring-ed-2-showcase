f2d0db8821cedb92dc5c79995d122184418eaab9
Ioannis Daniil <jondan97@gmail.com>
2022-04-07 12:24:47 +0300

    Added header binding for order reporting, added versions to order reporting

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/OrderController.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/OrderController.java
@@ -4,6 +4,8 @@ import gr.codelearn.spring.showcase.app.domain.Order;
 import gr.codelearn.spring.showcase.app.service.BaseService;
 import gr.codelearn.spring.showcase.app.service.OrderService;
 import gr.codelearn.spring.showcase.app.transfer.ApiResponse;
+import gr.codelearn.spring.showcase.app.transfer.KeyValue;
+import gr.codelearn.spring.showcase.app.transfer.PurchasesPerCustomerCategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -28,6 +31,66 @@ public class OrderController extends AbstractController<Order>{
 	@Override
 	public ResponseEntity<ApiResponse<List<Order>>> findAll() {
 		return new ResponseEntity<>(ApiResponse.<List<Order>>builder().data(orderService.findAllLazy()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(headers = "reporting=totalNumberAndCostOfPurchasesPerCustomerCategory")
+	public ResponseEntity<ApiResponse<List<PurchasesPerCustomerCategoryDto>>> totalNumberAndCostOfPurchasesPerCustomerCategory() {
+		return new ResponseEntity<>(ApiResponse.<List<PurchasesPerCustomerCategoryDto>>builder().data(orderService.findTotalNumberAndCostOfPurchasesPerCustomerCategory()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(headers = "reporting=averageOrderCostPerCustomer")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomer() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(orderService.findAverageOrderCostPerCustomer()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping("v1")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerV1() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(orderService.findAverageOrderCostPerCustomer()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping("v2")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerV2() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(null).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(headers = "X-API-VERSION=1")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerHV1() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(orderService.findAverageOrderCostPerCustomer()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(headers = "X-API-VERSION=2")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerHV2() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(null).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(params = "version=1")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerPV1() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(orderService.findAverageOrderCostPerCustomer()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(params = "version=2")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerPV2() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(null).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(produces = "application/vnd.api-v1+json")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerPPV1() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(orderService.findAverageOrderCostPerCustomer()).build(),
+									HttpStatus.OK);
+	}
+
+	@GetMapping(produces = "application/vnd.api-v2+json")
+	public ResponseEntity<ApiResponse<List<KeyValue<String, BigDecimal>>>> averageOrderCostPerCustomerPPV2() {
+		return new ResponseEntity<>(ApiResponse.<List<KeyValue<String, BigDecimal>>>builder().data(null).build(),
 									HttpStatus.OK);
 	}
 }
