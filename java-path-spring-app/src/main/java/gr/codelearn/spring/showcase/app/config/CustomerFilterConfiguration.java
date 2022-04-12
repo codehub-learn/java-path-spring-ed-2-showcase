803cf820adaa3a7ada32b000623033667efd5530
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-04-12 19:19:02 +0300

    Prefer multi-line instead of single line comments.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/config/CustomerFilterConfiguration.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/config/CustomerFilterConfiguration.java
@@ -9,18 +9,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomerFilterConfiguration {
-
 	@Bean
-	public Jackson2ObjectMapperBuilderCustomizer filterCustomizer(){
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.serializeAll();
+	public Jackson2ObjectMapperBuilderCustomizer jsonFilterCustomizer() {
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.serializeAllExcept("email");
 		FilterProvider filters = new SimpleFilterProvider().addFilter("customerFilter", filter);
+
 		// The following was replaced by a lambda expression:
-		//		return new Jackson2ObjectMapperBuilderCustomizer() {
-		//			@Override
-		//			public void customize(final Jackson2ObjectMapperBuilder builder) {
-		//				builder.filters(filters);
-		//			}
-		//		};
+		/*
+		return new Jackson2ObjectMapperBuilderCustomizer(){
+			@Override
+			public void customize(final Jackson2ObjectMapperBuilder builder) {
+				builder.filters(filters);
+			}
+		};
+		*/
+
 		return builder -> builder.filters(filters);
 	}
 }
